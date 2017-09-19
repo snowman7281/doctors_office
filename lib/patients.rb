51acd048f94cfa2cd6fdaff9a161d29c360490c7
Bably Kumari and Snow Vilay <bably@>
2017-09-19 10:58:36 -0700

    check for all the patients inside array of patients

--- lib/patients.rb
+++ lib/patients.rb
@@ -11,18 +11,17 @@ class Patients
    returned_patients.each() do |patient|
      name = patient.fetch("name")
      dr_id = patient.fetch("dr_id").to_i()
-     patient.push(Patients.new({:dr_id => dr_id, :name => name}))
+     patients.push(Patients.new({:dr_id => dr_id, :name => name}))
    end
    patients
  end
 
  def save
-   result = DB.exec("INSERT INTO patient (name) VALUES ('#{@name}') RETURNING dr_id;")
-   @dr_id = result.first().fetch("dr_id").to_i()
+   result = DB.exec("INSERT INTO patient (dr_id, name) VALUES ('#{@dr_id}', '#{@name}');")
  end
 
  def ==(another_patient)
-   self.name().==(another_patient.name()).&(self.id().==(another_patient.id()))
+   self.name().==(another_patient.name()).&(self.dr_id().==(another_patient.dr_id()))
  end
 
 end
---
+++ spec/patient_spec.rb
@@ -0,0 +1,27 @@
+require("rspec")
+require("pg")
+require("patients")
+
+DB = PG.connect({:dbname => 'doctors_office'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM patient *;")
+  end
+end
+
+describe(Patients) do
+  describe(".all") do
+    it("is empty at first") do
+      expect(Patients.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_patient = Patients.new({:dr_id => 1, :name => "Bably Kumari"})
+      test_patient.save()
+      expect(Patients.all()).to(eq([test_patient]))
+    end
+  end
+end
